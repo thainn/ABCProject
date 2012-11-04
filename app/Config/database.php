218745db7eeb254885a2d75ec6c830af751998af
thainn-leverages <thainn@leverages.jp>
2012-11-04 12:30:45 +0700

    thai

--- app/Config/database.php
+++ app/Config/database.php
@@ -60,24 +60,8 @@
 class DATABASE_CONFIG {
 
 	public $default = array(
-	AAAAAAAAAAAAAAAAAAAAA
-<<<<<<< HEAD
-		ádasdasd
-		a d
-		á d
-		á 
-		dá
-		 da
-		 sss
-=======
-			
-		'haitm' => 'Database/Mysql',	
-		'haitm' => 'Database/Mysql',
-		'haitm' => 'Database/Mysql',
-		'haitm' => 'Database/Mysql',
-		'persistent' => false,
-
->>>>>>> b580445e9eb2f3aa4f25333e5b8b94b5130120b2
+	AAAAAAAAAAAA
+	AAAAAAAAAAAA
 	);
 
 	public $test = array(
