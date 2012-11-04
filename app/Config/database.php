48fc7f25f8a1fc01399e333d9678e09e654f2e62
thainn-leverages <thainn@leverages.jp>
2012-11-04 16:18:51 +0700

    thai

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,12 +4,10 @@ class DATABASE_CONFIG {
 
 	public $default = array(
 
-<<<<<<< HEAD
-	Chẳng có gì hết ý chán thật
-=======
-		Chẳng có gì hết ý 
-	Test lần 2
->>>>>>> b358b5ad253b35f96b732514147dd39d7701a2e5
+	1A
+	2B
+	3C
+	
 
 	);
 
