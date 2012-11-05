58b5b33aca22216ca53fc95e64407512399030ed
thainn <tanngocson854@gmail.com>
2012-11-05 09:02:52 +0700

    thai

--- app/Config/Database5555.php
+++ app/Config/Database5555.php
@@ -4,11 +4,7 @@ class DATABASE_CONFIG {
 
 	public $default = array(
 	
-<<<<<<< HEAD:app/Config/Database4444.php
-thai leverages 6 6 
-=======
-thai leverages 5 5
->>>>>>> 7dcee9e94122654953e0175bf78c171300ac083b:app/Config/Database333.php
+test lần 1 
 
 	);
 
